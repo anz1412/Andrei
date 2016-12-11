5e558f846e6b45dfea26069e0bdeb4ac61e88f62
anz1412 <anzelika1412@mail.ru>
2016-12-10 21:45:04 -0500

    Add files via upload

---
+++ main.java
@@ -0,0 +1,10 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	      int number = 49;       // Set the value of "number" here!
+	      System.out.println("The number is " + number);
+	}
+
+}
